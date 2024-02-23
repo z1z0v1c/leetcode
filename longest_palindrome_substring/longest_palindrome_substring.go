a255ea2a7e3845c09ec9dec629c493f9a365c510
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-02-23 22:17:29 +0100

    5. Longest Palindromic Substring (Medium)

--- longest_palindrome_substring/longest_palindrome_substring.go
+++ longest_palindrome_substring/longest_palindrome_substring.go
@@ -1,5 +1,32 @@
 package longestpalindromesubstring
 
 func longestPalindrome(s string) string {
-    return ""
+	if len(s) <= 1 {
+		return s
+	}
+
+	lp := string(s[0])
+
+	for i := 0; i < len(s); i++ {
+		sub := string(s[i])
+		for j := i + 1; j < len(s); j++ {
+			sub += string(s[j])
+			if isPalindrome(&sub) {
+				if len(sub) > len(lp) {
+					lp = sub
+				}
+			}
+		}
+	}
+
+	return lp
+}
+
+func isPalindrome(s *string) bool {
+	for i := 0; i < len(*s)/2; i++ {
+		if (*s)[i] != (*s)[len(*s)-i-1] {
+			return false
+		}
+	}
+	return true
 }
--- longest_palindrome_substring/longest_palindrome_substring_test.go
+++ longest_palindrome_substring/longest_palindrome_substring_test.go
@@ -12,4 +12,103 @@ func TestLongestPalindrome(t *testing.T) {
 	if result != expected {
 		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
 	}
+
+	s = "a"
+	expected = "a"
+	result = longestPalindrome(s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
+	}
+
+	s = "ac"
+	expected = "a"
+	result = longestPalindrome(s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
+	}
+
+	s = "babad"
+	expected = "bab"
+	result = longestPalindrome(s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
+	}
+
+	s = "cbbd"
+	expected = "bb"
+	result = longestPalindrome(s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
+	}
+
+	s = "uzhynqvopdbnkvuxizirzjsslptlhmvyfqhqvqffmqldkrrdwapbdcxqbchvxqixmvbbtalrgzvkobyxlkonkfknabjwzeoazankqzuhexhcwkbvwtioubrcujqqeqoedhploqklhgeilwwnndbnzeadzefkcvaxdhgnmocadvvzjocoweyoidhleuwhmavpdiizbwkukpstyorlwwyiwwyyyzqqgipzzlxsgcdjscdfvrrrqmllbdjkkuisxeqaprfblvjuixajhucmcvffmevaztvadrujbthlnsdrxzvbldwxbazxmilpkbccigkihcgjbtpvignmrgzdqnufaacxtihfgwrllrwgfhitxcaafunqdzgrmngivptbjgchikgiccbkplimxzabxwdlbvzxrdsnlhtbjurdavtzavemffvcmcuhjaxiujvlbfrpaqexsiukkjdbllmqrrrvfdcsjdcgsxlzzpigqqzyyywwiywwlroytspkukwbziidpvamhwuelhdioyewocojzvvdacomnghdxavckfezdaeznbdnnwwlieghlkqolphdeoqeqqjucrbuoitwvbkwchxehuzqknazaoezwjbankfknoklxybokvzgrlatbbvmxiqxvhcbqxcdbpawdrrkdlqmffqvqhqfyvmhltplssjzrizixuvknbdpovqnyhzu"
+	expected = "uzhynqvopdbnkvuxizirzjsslptlhmvyfqhqvqffmqldkrrdwapbdcxqbchvxqixmvbbtalrgzvkobyxlkonkfknabjwzeoazankqzuhexhcwkbvwtioubrcujqqeqoedhploqklhgeilwwnndbnzeadzefkcvaxdhgnmocadvvzjocoweyoidhleuwhmavpdiizbwkukpstyorlwwyiwwyyyzqqgipzzlxsgcdjscdfvrrrqmllbdjkkuisxeqaprfblvjuixajhucmcvffmevaztvadrujbthlnsdrxzvbldwxbazxmilpkbccigkihcgjbtpvignmrgzdqnufaacxtihfgwrllrwgfhitxcaafunqdzgrmngivptbjgchikgiccbkplimxzabxwdlbvzxrdsnlhtbjurdavtzavemffvcmcuhjaxiujvlbfrpaqexsiukkjdbllmqrrrvfdcsjdcgsxlzzpigqqzyyywwiywwlroytspkukwbziidpvamhwuelhdioyewocojzvvdacomnghdxavckfezdaeznbdnnwwlieghlkqolphdeoqeqqjucrbuoitwvbkwchxehuzqknazaoezwjbankfknoklxybokvzgrlatbbvmxiqxvhcbqxcdbpawdrrkdlqmffqvqhqfyvmhltplssjzrizixuvknbdpovqnyhzu"
+	result = longestPalindrome(s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
+	}
+
+	s = "busislnescsicxpvvysuqgcudefrfjbwwjcchtgqyajdfwvkypfwshnihjdztgmyuuljxgvhdiwphrweyfkbnjgerkmifbirubhseuhrugwrabnjafnbdfjnufdstjbkuwtnpflffaqmjbhssjlnqftgjiglvvequhapasarlkcvbmkwnkuvwktbgfoaxteprobdwswcdyddyvrehvmxrrjiiidatidlpihkbmmruysmhhsncmfdanafdrfpdtfgkglcqpwrrtvacuicohspkounojuziittugpqjyhhkwfnflozbispehrtrnizowrlzcuollagxwtznjwzcumvedjwokueuqktvvouwnsmpxqvvpuwprezrbobrpnwaccwljchdguubjulyilzvmandjjleitweybqkjttschrjjlebnmponvlktzzcdtuybugggcqffkcffpamauvxfbonjrobgpvlyzveiwemmtdvbjciaytvesnocnjrwodtcokgcuoiicxapmrzpkfphjniuvzjrhbnqndfshoduejyktebgdabidxlkstepuwvtrtgbxaeheylicvhrxddijshcvdadxzsccmainyfpfdhqdanfccqkzlmhsfilvoybqojlvbcixjzqpbngdvesuokbxhkomsiqfyukvspqthlzxdnlwthrgaxhtpjzhrugqbfokrdcyurivmzgtynoqfjbafboselxnfupnpqlryvlcxeksirvufepfwczosrrjpudbwqxwldgjyfjhzlzcojxyqjyxxiqvfhjdwtgoqbyeocffnyxhyyiqspnvrpxmrtcnviukrjvpavervvztoxajriuvxqveqsrttjqepvvahywuzwtmgyrzduxfqspeipimyoxmkadrvrdyefekjxcmsmzmtbugyckcbjsrymszftjyllfmoeoylzeahnrxlxpnlvlvzltwnmldi"
+	expected = "rbobr"
+	result = longestPalindrome(s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %s, expected %s", s, result, expected)
+	}
+}
+
+func TestIsPalindrome(t *testing.T) {
+	s := ""
+	expected := true
+	result := isPalindrome(&s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %v, expected %v", s, result, expected)
+	}
+
+	s = "anavolimilovana"
+	expected = true
+	result = isPalindrome(&s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %v, expected %v", s, result, expected)
+	}
+
+	s = "ananevolimilovana"
+	expected = false
+	result = isPalindrome(&s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %v, expected %v", s, result, expected)
+	}
+
+	s = "wontloversrevoltnow"
+	expected = true
+	result = isPalindrome(&s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %v, expected %v", s, result, expected)
+	}
+
+	s = "loverswontrevoltnow"
+	expected = false
+	result = isPalindrome(&s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %v, expected %v", s, result, expected)
+	}
+
+	s = "uzhynqvopdbnkvuxizirzjsslptlhmvyfqhqvqffmqldkrrdwapbdcxqbchvxqixmvbbtalrgzvkobyxlkonkfknabjwzeoazankqzuhexhcwkbvwtioubrcujqqeqoedhploqklhgeilwwnndbnzeadzefkcvaxdhgnmocadvvzjocoweyoidhleuwhmavpdiizbwkukpstyorlwwyiwwyyyzqqgipzzlxsgcdjscdfvrrrqmllbdjkkuisxeqaprfblvjuixajhucmcvffmevaztvadrujbthlnsdrxzvbldwxbazxmilpkbccigkihcgjbtpvignmrgzdqnufaacxtihfgwrllrwgfhitxcaafunqdzgrmngivptbjgchikgiccbkplimxzabxwdlbvzxrdsnlhtbjurdavtzavemffvcmcuhjaxiujvlbfrpaqexsiukkjdbllmqrrrvfdcsjdcgsxlzzpigqqzyyywwiywwlroytspkukwbziidpvamhwuelhdioyewocojzvvdacomnghdxavckfezdaeznbdnnwwlieghlkqolphdeoqeqqjucrbuoitwvbkwchxehuzqknazaoezwjbankfknoklxybokvzgrlatbbvmxiqxvhcbqxcdbpawdrrkdlqmffqvqhqfyvmhltplssjzrizixuvknbdpovqnyhzu"
+	expected = true
+	result = isPalindrome(&s)
+
+	if result != expected {
+		t.Errorf("lengthOfLongestSubstring(%s) returned %v, expected %v", s, result, expected)
+	}
+
 }

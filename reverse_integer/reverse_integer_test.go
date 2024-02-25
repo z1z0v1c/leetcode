02920f82f7ac7b06a614a96accd789859e49aa84
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-02-25 19:07:11 +0100

    7. Reverse Integer (Medium) - Init commit

---
+++ reverse_integer/reverse_integer_test.go
@@ -0,0 +1,13 @@
+package reverseinteger
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	x := 0
+	expected := 0
+	result := reverse(x)
+
+	if result != expected {
+		t.Errorf("reverse(%d) returned %d, expected %d", x, result, expected)
+	}
+}

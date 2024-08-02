e6334552637cc730d5c4bae82d11d6860539d0fd
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-08-02 12:49:51 +0200

    mv all golang tasks to a separate folder

--- golang/string_to_integer/string_to_integer.go
+++ golang/string_to_integer/string_to_integer.go
@@ -3,7 +3,9 @@ package stringtointeger
 import "strings"
 
 func myAtoi(s string) int {
-	var zero byte = 0x30
+	var digits []byte = []byte{
+		0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+	}
 
 	s = strings.Trim(s, " ")
 	if len(s) == 0 {
@@ -20,8 +22,17 @@ func myAtoi(s string) int {
 		return 0
 	}
 
-	if s[0] == zero {
+	if s[0] == digits[0] {
 		return 0
+	}
+
+	var i int = 1
+	for j := range s {
+		for d := range digits {
+			if s[j] == digits[d] {
+				i *= d
+			}
+		}
 	}
 
 	if negative {

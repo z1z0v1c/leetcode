c27f202f04dd4abb7d3cfdec8832dc3fae0fe778
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-05 14:13:41 +0200

    pascals triangle ii

---
+++ java/src/main/java/pascalstriangleii/PascalsTriangleII.java
@@ -0,0 +1,45 @@
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it
+
+// Example 1:
+//      Input: rowIndex = 3
+//      Output: [1,3,3,1]
+//
+// Example 2:
+//      Input: rowIndex = 0
+//      Output: [1]
+//
+// Example 3:
+//      Input: rowIndex = 1
+//      Output: [1,1]
+//
+// Constraints:
+//      0 <= rowIndex <= 33
+
+package pascalstriangleii;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangleII {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        List<Integer> previousRow = new ArrayList<>();
+        previousRow.add(1);
+
+        for (int i = 0; i < rowIndex; i++) {
+            row.clear();
+
+            for(int j = 0; j <= previousRow.size(); j++) {
+                int previous = j == 0 ? 0 : previousRow.get(j - 1);
+                int next = j == previousRow.size() ? 0 : previousRow.get(j);
+                row.add(previous + next);
+            }
+
+            previousRow.clear();
+            previousRow.addAll(row);
+        }
+
+        return row.isEmpty() ? previousRow : row;
+    }
+}
---
+++ java/src/test/java/pascalstriangleii/PascalsTriangleIITest.java
@@ -0,0 +1,50 @@
+package pascalstriangleii;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PascalsTriangleIITest {
+    private PascalsTriangleII pascalsTriangleII;
+
+    @BeforeEach
+    void setUp() {
+        pascalsTriangleII = new PascalsTriangleII();
+    }
+
+    @Test
+    @DisplayName("Fourth row")
+    void testGetRowForFourthRow() {
+        int rowIndex = 3;
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 3, 3, 1));
+        List<Integer> actual = pascalsTriangleII.getRow(rowIndex);
+
+        assertEquals(expected, actual, "Should return [1, 3, 3, 1]");
+    }
+
+    @Test
+    @DisplayName("First row")
+    void testGetRowForFirstRow() {
+        int rowIndex = 0;
+        List<Integer> expected = new ArrayList<>(List.of(1));
+        List<Integer> actual = pascalsTriangleII.getRow(rowIndex);
+
+        assertEquals(expected, actual, "Should return [1]");
+    }
+
+    @Test
+    @DisplayName("Second row")
+    void testGetRowForSecondRow() {
+        int rowIndex = 1;
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 1));
+        List<Integer> actual = pascalsTriangleII.getRow(rowIndex);
+
+        assertEquals(expected, actual, "Should return [1, 1]");
+    }
+}

9b9a7ae16bdc9280c771dd6bb6faab53835fb64d
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-06 08:32:00 +0200

    pascals triangle

---
+++ java/src/main/java/pascalstriangle/PascalsTriangle.java
@@ -0,0 +1,44 @@
+// Difficulty - Easy
+//
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it
+//
+// Example 1:
+//      Input: numRows = 5
+//      Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//
+// Example 2:
+//      Input: numRows = 1
+//      Output: [[1]]
+//
+// Constraints:
+//      1 <= numRows <= 30
+
+package pascalstriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pyramid = new ArrayList<>();
+        List<Integer> nullRow = new ArrayList<>();
+        nullRow.add(1);
+
+        for (var i = 0; i < numRows; i++) {
+            List<Integer> previousRow = i == 0 ? nullRow : pyramid.get(i - 1);
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                int first = j == 0 ? 0 : previousRow.get(j - 1);
+                int second = j == previousRow.size() ? 0 : previousRow.get(j);
+
+                row.add(first + second);
+            }
+
+            pyramid.add(row);
+        }
+
+        return pyramid;
+    }
+}
---
+++ java/src/test/java/pascalstriangle/PascalsTriangleTest.java
@@ -0,0 +1,50 @@
+package pascalstriangle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PascalsTriangleTest {
+    private PascalsTriangle pascalsTriangle;
+
+    @BeforeEach
+    void setUp() {
+        pascalsTriangle = new PascalsTriangle();
+    }
+
+    @Test
+    @DisplayName("Five rows")
+    void testGenerateFiveRows() {
+        int rowIndex = 5;
+        var expected = new ArrayList<>(
+                Arrays.asList(
+                        List.of(1),
+                        Arrays.asList(1, 1),
+                        Arrays.asList(1, 2, 1),
+                        Arrays.asList(1, 3, 3, 1),
+                        Arrays.asList(1, 4, 6, 4, 1)
+                )
+        );
+        var actual = pascalsTriangle.generate(rowIndex);
+
+        assertEquals(expected, actual, "[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]");
+    }
+
+    @Test
+    @DisplayName("One row")
+    void testGenerateOneRow() {
+        int rowIndex = 1;
+        var expected = new ArrayList<>(
+                List.of(List.of(1))
+        );
+        var actual = pascalsTriangle.generate(rowIndex);
+
+        assertEquals(expected, actual, "[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]");
+    }
+}

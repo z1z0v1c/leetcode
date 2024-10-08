72b3f2b934e8d19e309befac5cd9a02b9f7fa967
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-09 00:08:27 +0200

    container with most water

---
+++ csharp/Tests/ContainerWithMostWater/ContainerWithMostWaterTests.cs
@@ -0,0 +1,36 @@
+using Solutions.ContainerWithMostWater;
+
+namespace ContainerWithMostWaterTests;
+
+public class ContainerWithMostWaterTests
+{
+    private ContainerWithMostWater containerWithMostWater;
+
+    [SetUp]
+    public void Setup()
+    {
+        containerWithMostWater = new ContainerWithMostWater();
+    }
+
+    [Test]
+    public void TestExampleOne()
+    {
+        int[] height = [1,8,6,2,5,4,8,3,7];
+
+        int expected = 49;
+        int actual = containerWithMostWater.MaxArea(height);
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        int[] height = [1,1];
+
+        int expected = 1;
+        int actual = containerWithMostWater.MaxArea(height);
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}

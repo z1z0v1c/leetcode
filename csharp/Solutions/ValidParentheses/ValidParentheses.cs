30f37cb65db7faca2c69d5bf80c5f889362cff04
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-09 07:56:17 +0200

    valid parentheses

---
+++ csharp/Solutions/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,77 @@
+namespace Solutions.ValidParentheses;
+
+/// <summary>
+/// 20 - Easy
+/// 
+/// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+/// determine if the input string is valid.
+/// 
+/// An input string is valid if:
+///      - Open brackets must be closed by the same type of brackets.
+///      - Open brackets must be closed in the correct order.
+///      - Every close bracket has a corresponding open bracket of the same type.
+///  
+/// Example 1:
+///      Input: s = "()"
+///      Output: true
+/// 
+/// Example 2:
+///      Input: s = "()[]{}"
+///      Output: true
+/// 
+/// Example 3:
+///      Input: s = "(]"
+///      Output: false
+/// 
+/// Example 4:
+///      Input: s = "([])"
+///      Output: true
+/// 
+/// Constraints:
+///      - 1 <= s.length <= 104
+///      - s consists of parentheses only '()[]{}'.
+/// </summary>
+
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                stack.Push(s[i]);
+            }
+            else if (s[i] == ')' || s[i] == '}' || s[i] == ']')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                char c = stack.Peek();
+
+                if (
+                    (c == '(' && s[i] == ')') ||
+                    (c == '{' && s[i] == '}') ||
+                    (c == '[' && s[i] == ']'))
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.Count != 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ csharp/Tests/ValidParentheses/ValidParenthesesTests.cs
@@ -0,0 +1,43 @@
+using Solutions.ValidParentheses;
+
+namespace Tests.ValidParenthesesTests;
+
+public class ValidParenthesesTests
+{
+    private ValidParentheses validParentheses;
+
+    [SetUp]
+    public void Setup() => validParentheses = new ValidParentheses();
+
+    [Test]
+    public void TestExampleOne()
+    {
+        string s = "()";
+
+        Assert.That(validParentheses.IsValid(s), Is.True);
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        string s = "()[]{}";
+
+        Assert.That(validParentheses.IsValid(s), Is.True);
+    }
+
+    [Test]
+    public void TestExampleThree()
+    {
+        string s = "(]";
+
+        Assert.That(validParentheses.IsValid(s), Is.False);
+    }
+
+    [Test]
+    public void TestExampleFour()
+    {
+        string s = "([])";
+
+        Assert.That(validParentheses.IsValid(s), Is.True);
+    }
+}

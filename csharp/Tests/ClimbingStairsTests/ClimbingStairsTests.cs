7bfb135b9d0ed4125a30a99caf2cece8730007c8
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-09 11:14:41 +0200

    climbing stairs

---
+++ csharp/Tests/ClimbingStairsTests/ClimbingStairsTests.cs
@@ -0,0 +1,33 @@
+using Solutions.ClimbingStairs;
+
+namespace Tests.ClimbingStairsTests;
+
+public class ClimbingStairsTests
+{
+    private ClimbingStairs climbingStairs;
+
+    [SetUp]
+    public void Setup() => climbingStairs = new();
+
+    [Test]
+    public void TestExampleOne()
+    {
+        int n = 2;
+
+        int expected = 2;
+        int actual = climbingStairs.ClimbStairs(n);
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        int n = 3;
+
+        int expected = 3;
+        int actual = climbingStairs.ClimbStairs(n);
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}

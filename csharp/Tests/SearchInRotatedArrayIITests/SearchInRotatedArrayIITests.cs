32ea4d40a5b3a6a93246174762fa24d1641d8b52
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-09 11:27:56 +0200

    search in rotated sorted array ii

---
+++ csharp/Tests/SearchInRotatedArrayIITests/SearchInRotatedArrayIITests.cs
@@ -0,0 +1,29 @@
+using Solutions.SearchInRotatedArrayII;
+
+namespace Tests.SearchInRotatedArrayIITests;
+
+public class SearchInRotatedArrayIITests
+{
+    private SearchInRotatedArrayII searchInRotatedArrayII;
+
+    [SetUp]
+    public void Setup() => searchInRotatedArrayII = new();
+
+    [Test]
+    public void TestExampleOne()
+    {
+        int[] nums = [2,5,6,0,0,1,2];
+        int target = 0;
+        
+        Assert.That(searchInRotatedArrayII.Search(nums, target), Is.True);
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        int[] nums = [2,5,6,0,0,1,2];
+        int target = 3;
+        
+        Assert.That(searchInRotatedArrayII.Search(nums, target), Is.False);
+    }
+}

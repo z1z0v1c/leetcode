45b4f0b9648bde9c1c8a0bf095d0a4f86aab3eac
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-09 07:42:40 +0200

    serial number  and diificulty for lcp

--- csharp/Solutions/LongestCommonPrefix/LongestCommonPrefix.cs
+++ csharp/Solutions/LongestCommonPrefix/LongestCommonPrefix.cs
@@ -1,6 +1,8 @@
 namespace Solutions.LongestCommonPrefix;
 
 /// <summary>
+/// 14 - Easy
+/// 
 /// Write a function to find the longest common prefix string amongst an array of strings.
 /// If there is no common prefix, return an empty string "".
 /// 

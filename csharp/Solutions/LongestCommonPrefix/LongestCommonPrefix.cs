7fcf3603dd9a8e7ec5a598fd453fdf1f838d421e
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-09 07:39:23 +0200

    longest common prefix

---
+++ csharp/Solutions/LongestCommonPrefix/LongestCommonPrefix.cs
@@ -0,0 +1,42 @@
+namespace Solutions.LongestCommonPrefix;
+
+/// <summary>
+/// Write a function to find the longest common prefix string amongst an array of strings.
+/// If there is no common prefix, return an empty string "".
+/// 
+/// Example 1:
+///      Input: strs = ["flower","flow","flight"]
+///      Output: "fl"
+/// 
+/// Example 2:
+///      Input: strs = ["dog","racecar","car"]
+///      Output: ""
+///      Explanation: There is no common prefix among the input strings.
+///  
+/// Constraints:
+///      - 1 <= strs.length <= 200
+///      - 0 <= strs[i].length <= 200
+///      - strs[i] consists of only lowercase English letters.
+/// </summary>
+
+public class LongestCommonPrefixSolution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = "";
+
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            for (int j = 0; j < strs.Length; j ++)
+            {
+                if (i >= strs[j].Length || strs[0][i] != strs[j][i])
+                {
+                    return prefix;
+                }                    
+            }
+                prefix += strs[0][i];
+        }
+
+        return prefix;
+    }
+}
---
+++ csharp/Tests/LongestCommonPrefix/LongestCommonPrefixTests.cs
@@ -0,0 +1,36 @@
+using Solutions.LongestCommonPrefix;
+
+namespace Tests.LongestCommonPrefixTests;
+
+public class LittleLongestCommonPrefixTests
+{
+    private LongestCommonPrefixSolution longestCommonPrefix;
+
+    [SetUp]
+    public void Setup()
+    {
+        longestCommonPrefix = new LongestCommonPrefixSolution();
+    }
+
+    [Test]
+    public void TestExampleOne()
+    {
+        string[] strs = ["flower","flow","flight"];
+
+        string expected = "fl";
+        string actual = longestCommonPrefix.LongestCommonPrefix(strs);
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        string[] strs = ["dog","racecar","car"];
+
+        string expected = "";
+        string actual = longestCommonPrefix.LongestCommonPrefix(strs);
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}

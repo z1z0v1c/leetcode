1d8914d041d2d6c056d1cb0a949b506deb7223bd
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-11 08:04:34 +0200

    final three sum closest

--- java/src/main/java/threesumclosest/ThreeSumClosest.java
+++ java/src/main/java/threesumclosest/ThreeSumClosest.java
@@ -10,19 +10,19 @@ import java.util.*;
  * You may assume that each input would have exactly one solution.
  * <p>
  * Example 1:
- * Input: nums = [-1,2,1,-4], target = 1
- * Output: 2
- * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *      Input: nums = [-1,2,1,-4], target = 1
+ *      Output: 2
+ *      Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  * <p>
  * Example 2:
- * Input: nums = [0,0,0], target = 1
- * Output: 0
- * Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ *      Input: nums = [0,0,0], target = 1
+ *      Output: 0
+ *      Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
  * <p>
  * Constraints:
- * - 3 <= nums.length <= 500
- * - -1000 <= nums[i] <= 1000
- * - -104 <= target <= 104
+ *      - 3 <= nums.length <= 500
+ *      - -1000 <= nums[i] <= 1000
+ *      - -104 <= target <= 104
  */
 
 public class ThreeSumClosest {

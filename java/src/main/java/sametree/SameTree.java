2d62e25f4c1e24fbbdce2e0c1294a134a9071cc5
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-11 17:15:28 +0200

    same tree - init commit

---
+++ java/src/main/java/sametree/SameTree.java
@@ -0,0 +1,31 @@
+package sametree;
+
+import commonclasses.TreeNode;
+
+/**
+ * 100 - Easy
+ * <p>
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ * <p>
+ * Example 1:
+ *      Input: p = [1,2,3], q = [1,2,3]
+ *      Output: true
+ * <p>
+ * Example 2:
+ *      Input: p = [1,2], q = [1,null,2]
+ *      Output: false
+ * <p>
+ * Example 3:
+ *      Input: p = [1,2,1], q = [1,1,2]
+ *      Output: false
+ * <p>
+ * Constraints:
+ *      - The number of nodes in both trees is in the range [0, 100].
+ *      - -104 <= Node.val <= 104
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return false;
+    }
+}
---
+++ java/src/test/java/sametree/SameTreeTest.java
@@ -0,0 +1,71 @@
+package sametree;
+
+import commonclasses.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SameTreeTest {
+    private SameTree sameTree;
+
+    @BeforeEach
+    void setUp() {
+        sameTree = new SameTree();
+    }
+
+    @Test
+    @DisplayName("Example one")
+    void testExampleOne() {
+        var p = new TreeNode(
+                1,
+                new TreeNode(2),
+                null
+        );
+
+        var q = new TreeNode(
+                1,
+                null,
+                new TreeNode(2)
+        );
+
+        assertFalse(sameTree.isSameTree(p, q));
+    }
+
+    @Test
+    @DisplayName("Example two")
+    void testExampleTwo() {
+        var p = new TreeNode(
+                1,
+                new TreeNode(2),
+                new TreeNode(3)
+        );
+
+        var q = new TreeNode(
+                1,
+                new TreeNode(2),
+                new TreeNode(3)
+        );
+
+        assertTrue(sameTree.isSameTree(p, q));
+    }
+
+    @Test
+    @DisplayName("Example three")
+    void testExampleThree() {
+        var p = new TreeNode(
+                1,
+                new TreeNode(2),
+                null
+        );
+
+        var q = new TreeNode(
+                1,
+                null,
+                new TreeNode(2)
+        );
+
+        assertFalse(sameTree.isSameTree(p, q));
+    }
+}

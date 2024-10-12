2863346c8d4500c711beafeaa98cfdff95abeda6
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-12 21:09:06 +0200

    definition for singly-linked list

---
+++ java/src/main/java/commonclasses/ListNode.java
@@ -0,0 +1,14 @@
+package commonclasses;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}

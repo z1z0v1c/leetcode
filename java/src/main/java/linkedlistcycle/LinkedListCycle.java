69d7e7cf0872f7489a079f7e77371787c30af4e2
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-12 21:25:58 +0200

    linked list cycle - init commit

---
+++ java/src/main/java/linkedlistcycle/LinkedListCycle.java
@@ -0,0 +1,42 @@
+package linkedlistcycle;
+
+import commonclasses.ListNode;
+
+/**
+ * 141 - Easy
+ * <p>
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again
+ * by continuously following the next pointer. Internally, pos is used to denote the index of the
+ * node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+ * <p>
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * <p>
+ * Example 1:
+ *      Input: head = [3,2,0,-4], pos = 1
+ *      Output: true
+ *      Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ * <p>
+ * Example 2:
+ *      Input: head = [1,2], pos = 0
+ *      Output: true
+ *      Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+ * <p>
+ * Example 3:
+ *      Input: head = [1], pos = -1
+ *      Output: false
+ *      Explanation: There is no cycle in the linked list.
+ * <p>
+ * Constraints:
+ *      - The number of the nodes in the list is in the range [0, 10^4].
+ *      - -10^5 <= Node.val <= 10^5
+ *      - pos is -1 or a valid index in the linked-list.
+ * <p>
+ * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ */
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        return false;
+    }
+}
---
+++ java/src/test/java/linkedlistcycle/LinkedListCycleTest.java
@@ -0,0 +1,70 @@
+package linkedlistcycle;
+
+import commonclasses.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListCycleTest {
+    private LinkedListCycle linkedListCycle;
+
+    @BeforeEach
+    void setUp() {
+        linkedListCycle = new LinkedListCycle();
+    }
+
+    @Test
+    @DisplayName("Example 1.")
+    void testExampleOne() {
+        var head = new ListNode(3);
+        var nodeOne = new ListNode(2);
+        var nodeTwo = new ListNode(1);
+        var nodeThree = new ListNode(-4);
+
+        head.next = nodeOne;
+        nodeOne.next = nodeTwo;
+        nodeTwo.next = nodeThree;
+        nodeThree.next = nodeOne;
+
+        assertTrue(linkedListCycle.hasCycle(head));
+    }
+
+    @Test
+    @DisplayName("Example 2.")
+    void testExampleTwo() {
+        var head = new ListNode(1);
+        var nodeOne = new ListNode(2);
+
+        head.next = nodeOne;
+        nodeOne.next = head;
+
+        assertTrue(linkedListCycle.hasCycle(head));
+    }
+
+    @Test
+    @DisplayName("Example 3.")
+    void testExampleThree() {
+        var head = new ListNode(1);
+
+        assertFalse(linkedListCycle.hasCycle(head));
+    }
+
+    @Test
+    @DisplayName("Example 4.")
+    void testExampleFour() {
+        var head = new ListNode(7);
+        var nodeOne = new ListNode(5);
+        var nodeTwo = new ListNode(9);
+        var nodeThree = new ListNode(-3);
+        var nodeFour = new ListNode(1);
+
+        head.next = nodeOne;
+        nodeOne.next = nodeTwo;
+        nodeTwo.next = nodeThree;
+        nodeThree.next = nodeFour;
+
+        assertFalse(linkedListCycle.hasCycle(head));
+    }
+}

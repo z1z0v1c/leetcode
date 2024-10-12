629f2f61224265b754a274007b8e375abd8c02e0
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-12 17:44:40 +0200

    valid palindrome - init commit

---
+++ java/src/test/java/validpalindrome/ValidPalindromeTest.java
@@ -0,0 +1,40 @@
+package validpalindrome;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidPalindromeTest {
+    private ValidPalindrome validPalindrome;
+
+    @BeforeEach
+    void setUp() {
+        validPalindrome = new ValidPalindrome();
+    }
+
+    @Test
+    @DisplayName("Example 1.")
+    void testExampleOne() {
+        String s = "A man, a plan, a canal: Panama";
+
+        assertTrue(validPalindrome.isPalindrome(s));
+    }
+
+    @Test
+    @DisplayName("Example 2.")
+    void testExampleTwo() {
+        String s = "race a car";
+
+        assertFalse(validPalindrome.isPalindrome(s));
+    }
+
+    @Test
+    @DisplayName("Example 3.")
+    void testExampleThree() {
+        String s = "  ";
+
+        assertTrue(validPalindrome.isPalindrome(s));
+    }
+}

923eaed0b12aa6ea2ee690eea1e9b2a0b1d2b416
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-12 23:16:53 +0200

    excel sheet column title - init commit

---
+++ java/src/main/java/excelsheetcolumntitle/ExcelSheetColumnTitle.java
@@ -0,0 +1,39 @@
+package excelsheetcolumntitle;
+
+/**
+ * 168 - Easy
+ * <p>
+ * Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+ * <p>
+ * For example:
+ * <p>
+ *      A -> 1
+ *      B -> 2
+ *      C -> 3
+ *      ...
+ *      Z -> 26
+ *      AA -> 27
+ *      AB -> 28
+ *      ...
+ * <p>
+ * Example 1:
+ *      Input: columnNumber = 1
+ *      Output: "A"
+ * <p>
+ * Example 2:
+ *      Input: columnNumber = 28
+ *      Output: "AB"
+ * <p>
+ * Example 3:
+ *      Input: columnNumber = 701
+ *      Output: "ZY"
+ * <p>
+ * Constraints:
+ *      - 1 <= columnNumber <= 231 - 1
+ */
+
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int columnNumber) {
+        return null;
+    }
+}
---
+++ java/src/test/java/excelsheetcolumntitle/ExcelSheetColumnTitleTest.java
@@ -0,0 +1,49 @@
+package excelsheetcolumntitle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExcelSheetColumnTitleTest {
+    private ExcelSheetColumnTitle excelSheetColumnTitle;
+
+    @BeforeEach
+    void setUp() {
+        excelSheetColumnTitle = new ExcelSheetColumnTitle();
+    }
+
+    @Test
+    @DisplayName("Example 1.")
+    void testExampleOne() {
+        var columnNumber = 1;
+
+        var expected = "A";
+        var actual = excelSheetColumnTitle.convertToTitle(columnNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 2.")
+    void testExampleTwo() {
+        var columnNumber = 28;
+
+        var expected = "AB";
+        var actual = excelSheetColumnTitle.convertToTitle(columnNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 3.")
+    void testExampleThree() {
+        var columnNumber = 701;
+
+        var expected = "ZY";
+        var actual = excelSheetColumnTitle.convertToTitle(columnNumber);
+
+        assertEquals(expected, actual);
+    }
+}

7808e082bed2f29e0afe941ef3387c10714a389b
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-12 18:16:12 +0200

    single number - init commit

---
+++ java/src/main/java/singlenumber/SingleNumber.java
@@ -0,0 +1,31 @@
+package singlenumber;
+
+/**
+ * 136 - Easy
+ * <p>
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ * <p>
+ * Example 1:
+ *      Input: nums = [2,2,1]
+ *      Output: 1
+ * <p>
+ * Example 2:
+ *      Input: nums = [4,1,2,1,2]
+ *      Output: 4
+ * <p>
+ * Example 3:
+ *      Input: nums = [1]
+ *      Output: 1
+ * <p>
+ * Constraints:
+ *      - 1 <= nums.length <= 3 * 104
+ *      - -3 * 104 <= nums[i] <= 3 * 104
+ *      - Each element in the array appears twice except for one element which appears only once.
+ */
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        return 0;
+    }
+}
---
+++ java/src/test/java/singlenumber/SingleNumberTest.java
@@ -0,0 +1,48 @@
+package singlenumber;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleNumberTest {
+    private SingleNumber singleNumber;
+    @BeforeEach
+    void setUp() {
+        singleNumber = new SingleNumber();
+    }
+
+    @Test
+    @DisplayName("Example 1.")
+    void testExampleOne() {
+        int[] nums = {2,2,1};
+
+        int expected = 1;
+        int actual = singleNumber.singleNumber(nums);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 2.")
+    void testExampleTwo() {
+        int[] nums = {4,1,2,1,2};
+
+        int expected = 4;
+        int actual = singleNumber.singleNumber(nums);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 3.")
+    void testExampleThree() {
+        int[] nums = {1};
+
+        int expected = 1;
+        int actual = singleNumber.singleNumber(nums);
+
+        assertEquals(expected, actual);
+    }
+}

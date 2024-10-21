44f08c2a135a342ea1f51b7430b1c9079d5eaaa8
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-21 11:34:28 +0200

    happy number - init commit

---
+++ java/src/test/java/happynumber/HappyNumberTest.java
@@ -0,0 +1,32 @@
+package happynumber;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HappyNumberTest {
+    private HappyNumber happyNumber;
+
+    @BeforeEach
+    void setUp() {
+        happyNumber = new HappyNumber();
+    }
+
+    @Test
+    @DisplayName("Example 1.")
+    void testExampleOne() {
+        int n = 19;
+
+        assertTrue(happyNumber.isHappy(n));
+    }
+
+    @Test
+    @DisplayName("Example 2.")
+    void testExampleTwo() {
+        int n = 2;
+
+        assertFalse(happyNumber.isHappy(n));
+    }
+}

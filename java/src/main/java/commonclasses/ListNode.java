79a93137158c5cb0846096c531885de3ae6888c8
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-22 08:51:15 +0200

    add a constructor to the list node class

--- java/src/main/java/commonclasses/ListNode.java
+++ java/src/main/java/commonclasses/ListNode.java
@@ -8,7 +8,11 @@ public class ListNode {
     public ListNode next;
 
     public ListNode(int x) {
-        val = x;
-        next = null;
+        this.val = x;
+    }
+
+    public ListNode(int x, ListNode next) {
+        this.val = x;
+        this.next = next;
     }
 }

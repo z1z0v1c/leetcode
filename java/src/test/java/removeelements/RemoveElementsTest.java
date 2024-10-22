aa4c8ca6881af1cab3972499714c676680bf4101
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-10-22 08:52:00 +0200

    remove linked list elements - init commit

---
+++ java/src/test/java/removeelements/RemoveElementsTest.java
@@ -0,0 +1,69 @@
+package removeelements;
+
+import commonclasses.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveElementsTest {
+    private RemoveElements removeElements;
+
+    @BeforeEach
+    void setUp() {
+        removeElements = new RemoveElements();
+    }
+
+    @Test
+    @DisplayName("Example 1.")
+    void testExampleOne() {
+        int val = 6;
+        var head =
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(6,
+                                        new ListNode(3,
+                                                new ListNode(4,
+                                                        new ListNode(5,
+                                                                new ListNode(6)))))));
+
+        var expected =
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(3,
+                                        new ListNode(4,
+                                                new ListNode(5)))));
+
+        var actual = removeElements.removeElements(head, val);
+
+        while (actual.next != null) {
+            assertEquals(expected.val, actual.val);
+
+            expected = expected.next;
+            actual = actual.next;
+        }
+    }
+
+    @Test
+    @DisplayName("Example 2.")
+    void testExampleTwo() {
+        int val = 1;
+        ListNode head = null;
+
+        assertNull(removeElements.removeElements(head, val));
+    }
+
+    @Test
+    @DisplayName("Example 3.")
+    void testExampleThree() {
+        int val = 7;
+        var head =
+                new ListNode(7,
+                        new ListNode(7,
+                                new ListNode(7,
+                                        new ListNode(7))));
+
+        assertNull(removeElements.removeElements(head, val));
+    }
+}

e13e7abf78eb03eb1a5450f444564233f4072942
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-11-24 15:35:17 +0100

    customer placing the largest number of orders - solution

---
+++ sql/customer_largest_number_of_orders.sql
@@ -0,0 +1,67 @@
+/*
+
+586 - Easy
+
+Table: Orders
+
+	+-----------------+----------+
+	| Column Name     | Type     |
+	+-----------------+----------+
+	| order_number    | int      |
+	| customer_number | int      |
+	+-----------------+----------+
+
+	order_number is the primary key (column with unique values) for this table.
+	This table contains information about the order ID and the customer ID.
+ 
+
+Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+The result format is in the following example.
+ 
+
+Example 1:
+
+	Input: 
+
+		Orders table:
+		+--------------+-----------------+
+		| order_number | customer_number |
+		+--------------+-----------------+
+		| 1            | 1               |
+		| 2            | 2               |
+		| 3            | 3               |
+		| 4            | 3               |
+		+--------------+-----------------+
+
+	Output: 
+		+-----------------+
+		| customer_number |
+		+-----------------+
+		| 3               |
+		+-----------------+
+
+	Explanation: 
+
+		- The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+		- So the result is customer_number 3.
+*/
+
+with order_counts(customer_number, order_count) as (
+    select
+        customer_number,
+        count(*)
+    from 
+        Orders
+    group by
+        customer_number
+)
+select
+    customer_number
+from
+    order_counts
+order by 
+    order_count
+desc
+limit 1
+

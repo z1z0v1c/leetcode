14487057e9a71d81f56a03735bd56a734bf6d8b9
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-05 22:55:20 +0100

    string to integer - add tests

--- golang/string_to_integer/string_to_integer_test.go
+++ golang/string_to_integer/string_to_integer_test.go
@@ -36,7 +36,7 @@ func TestMyAtoi(t *testing.T) {
 	}
 
 	s = "  -=   "
-	expected = -1
+	expected = 0
 	result = myAtoi(s)
 
 	if result != expected {
@@ -44,7 +44,7 @@ func TestMyAtoi(t *testing.T) {
 	}
 
 	s = "  =   "
-	expected = 1
+	expected = 0
 	result = myAtoi(s)
 
 	if result != expected {
@@ -52,6 +52,46 @@ func TestMyAtoi(t *testing.T) {
 	}
 
 	s = "  -0  "
+	expected = 0
+	result = myAtoi(s)
+
+	if result != expected {
+		t.Errorf("reverse(%s) returned %d, expected %d", s, result, expected)
+	}
+
+	s = "42"
+	expected = 42
+	result = myAtoi(s)
+
+	if result != expected {
+		t.Errorf("reverse(%s) returned %d, expected %d", s, result, expected)
+	}
+
+	s = " -042"
+	expected = -42
+	result = myAtoi(s)
+
+	if result != expected {
+		t.Errorf("reverse(%s) returned %d, expected %d", s, result, expected)
+	}
+
+	s = "1337c0d3"
+	expected = 1337
+	result = myAtoi(s)
+
+	if result != expected {
+		t.Errorf("reverse(%s) returned %d, expected %d", s, result, expected)
+	}
+
+	s = "0-1"
+	expected = 0
+	result = myAtoi(s)
+
+	if result != expected {
+		t.Errorf("reverse(%s) returned %d, expected %d", s, result, expected)
+	}
+
+	s = "words and 987"
 	expected = 0
 	result = myAtoi(s)
 

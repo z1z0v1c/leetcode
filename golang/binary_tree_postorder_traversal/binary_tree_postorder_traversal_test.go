d5ba1c8deba2a3956d2bcb72d9c0e588a2cd4426
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-09 09:21:47 +0100

    binary tree postorder traversal - add tests

---
+++ golang/binary_tree_postorder_traversal/binary_tree_postorder_traversal_test.go
@@ -0,0 +1,64 @@
+package binarytreepostordertraversal
+
+import (
+	"slices"
+	"testing"
+
+	cs "github.com/z1z0v1c/leetcode/commonstructs"
+)
+
+func TestPostorderTraversal(t *testing.T) {
+	tn0 := &cs.TreeNode{Val: 1}
+	tn1 := &cs.TreeNode{Val: 2}
+	tn2 := &cs.TreeNode{Val: 3}
+
+	tn0.Right = tn1
+	tn1.Left = tn2
+
+	expected := []int{3, 2, 1}
+	actual := postorderTraversal(tn0)
+
+	if !slices.Equal(actual, expected) {
+		t.Errorf("postorderTraversal() returned incorrect value.")
+	}
+
+	tn0 = &cs.TreeNode{Val: 1}
+	tn1 = &cs.TreeNode{Val: 2}
+	tn2 = &cs.TreeNode{Val: 3}
+	tn3 := &cs.TreeNode{Val: 4}
+	tn4 := &cs.TreeNode{Val: 5}
+	tn5 := &cs.TreeNode{Val: 6}
+	tn6 := &cs.TreeNode{Val: 7}
+	tn7 := &cs.TreeNode{Val: 8}
+	tn8 := &cs.TreeNode{Val: 9}
+
+	tn0.Left = tn1
+	tn0.Right = tn2
+	tn1.Left = tn3
+	tn1.Right = tn4
+	tn4.Left = tn5
+	tn5.Right = tn6
+	tn2.Left = tn7
+	tn2.Right = tn8
+
+	expected = []int{4, 6, 7, 5, 2, 9, 8, 3, 1}
+	actual = postorderTraversal(tn0)
+
+	if !slices.Equal(actual, expected) {
+		t.Errorf("postorderTraversal() returned incorrect value.")
+	}
+
+	tn0 = &cs.TreeNode{Val: 1}
+
+	expected = []int{1}
+	actual = postorderTraversal(tn0)
+
+	if !slices.Equal(actual, expected) {
+		t.Errorf("postorderTraversal() returned incorrect value.")
+	}
+
+	// Test nil input
+	if postorderTraversal(nil) != nil {
+		t.Errorf("postorderTraversal(nil) should return nil.")
+	}
+}

9cb3dc97f6f1d5e86f1bc7b248f6fadfc33f600d
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-11 12:30:52 +0100

    valid palindrome ii - add tests

---
+++ golang/680_valid_palindrome_ii/valid_palindrome_ii_test.go
@@ -0,0 +1,26 @@
+package validpalindromeii
+
+import "testing"
+
+func TestValidPalindrome(t *testing.T) {
+	s := "aba"
+	result := validPalindrome(s)
+
+	if !result {
+		t.Errorf("validPalindrome(%s) should return true.", s)
+	}
+
+	s = "abca"
+	result = validPalindrome(s)
+
+	if !result {
+		t.Errorf("validPalindrome(%s) should return true.", s)
+	}
+
+	s = "abc"
+	result = validPalindrome(s)
+
+	if result {
+		t.Errorf("validPalindrome(%s) should return false.", s)
+	}
+}

38d27573ff3c68a08bd2583dce0c050967b76419
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-29 16:02:49 +0100

    binary search - add third test case

--- golang/binarysearch/binary_search_test.go
+++ golang/binarysearch/binary_search_test.go
@@ -3,6 +3,7 @@ package binarysearch
 import "testing"
 
 func TestSearch(t *testing.T) {
+	// Example one
 	nums := []int{-1, 0, 3, 5, 9, 12}
 	target := 9
 
@@ -13,10 +14,22 @@ func TestSearch(t *testing.T) {
 		t.Errorf("Error: serch(nums) should returned %d instead %d.", actual, expected)
 	}
 
+	// Example two
 	nums = []int{-1, 0, 3, 5, 9, 12}
 	target = 2
 
 	expected = -1
+	actual = search(nums, target)
+
+	if expected != actual {
+		t.Errorf("Error: serch(nums) should returned %d instead %d.", actual, expected)
+	}
+
+	// Example three
+	nums = []int{5}
+	target = 5
+
+	expected = 0
 	actual = search(nums, target)
 
 	if expected != actual {

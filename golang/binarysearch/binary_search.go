d1a2b0b2054e931e73ef4fe868f0c48587203969
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-31 09:06:34 +0100

    binary search - final solution

--- golang/binarysearch/binary_search.go
+++ golang/binarysearch/binary_search.go
@@ -1,17 +1,17 @@
-/* 
+/*
 704 - Easy
 
 Given an array of integers nums which is sorted in ascending order, and an integer target,
 write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
-You must write an algorithm with O(log n) runtime complexity. 
+You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 
 	Input: nums = [-1,0,3,5,9,12], target = 9
 	Output: 4
 	Explanation: 9 exists in nums and its index is 4
-	
+
 Example 2:
 
 	Input: nums = [-1,0,3,5,9,12], target = 2
@@ -28,5 +28,19 @@ Constraints:
 package binarysearch
 
 func search(nums []int, target int) int {
-	return len(nums) - target
+	start := 0
+	end := len(nums) - 1
+
+	for start <= end {
+		middle := (start + end) / 2
+		if target < nums[middle] {
+			end = middle - 1
+		} else if target > nums[middle] {
+			start = middle + 1
+		} else {
+			return middle
+		}
+	}
+
+	return -1
 }

f32a0fbafdf71a9a9ea8feeca80a2831f5234771
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-10 18:02:57 +0100

    min cost climbing stairs - init commit

---
+++ golang/mincostclimbingstairs/min_cost_climbing_stairs_test.go
@@ -0,0 +1,25 @@
+package mincostclimbingstairs
+
+import "testing"
+
+func TestMinCostClimbingStairs(t *testing.T) {
+	// Example one
+	cost := []int{10, 25, 20}
+	
+	expected := 15
+	actual := minCostClimbingStairs(cost)
+
+	if expected != actual {
+		t.Errorf("minCostClimbingStairs(cost) returned %d instead %d.", actual, expected)
+	}
+
+	// Example two
+	cost = []int{1,100,1,1,1,100,1,1,100,1}
+	
+	expected = 6
+	actual = minCostClimbingStairs(cost)
+
+	if expected != actual {
+		t.Errorf("minCostClimbingStairs(cost) returned %d instead %d.", actual, expected)
+	}
+}

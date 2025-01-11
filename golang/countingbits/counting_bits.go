31978388710327759a9b9c90be24a67050b54373
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-11 21:18:12 +0100

    counting bits - math/bits solution

--- golang/countingbits/counting_bits.go
+++ golang/countingbits/counting_bits.go
@@ -1,7 +1,8 @@
-/**
- * 338 - Easy
- *
- * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+/*
+*
+  - 338 - Easy
+    *
+  - Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 Example 1:
 
@@ -28,16 +29,42 @@ Example 2:
 
 Constraints:
 
-	- 0 <= n <= 10^5
+  - 0 <= n <= 10^5
 
 Follow up:
 
 	It is very easy to come up with a solution with a runtime of O(n log n).
 	Can you do it in linear time O(n) and possibly in a single pass?
 	Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
- */
+*/
 package countingbits
 
 func countBits(n int) []int {
-	return []int{n}
+	counts := []int{}
+
+	for i := 0; i <= n; i++ {
+		counts = append(counts, 0)
+		num := i
+
+		for num > 0 {
+			counts[i] += num & 1
+			num >>= 1
+		}
+	} 
+
+	return counts
 }
+
+/**
+import "math/bits"
+
+func countBits(n int) []int {
+	counts := []int{}
+
+	for i := 0; i <= n; i++ {
+		counts = append(counts, bits.OnesCount(uint(i)))
+	} 
+
+	return counts
+}
+*/
--- golang/countingbits/counting_bits_test.go
+++ golang/countingbits/counting_bits_test.go
@@ -11,16 +11,16 @@ func TestCountBits(t *testing.T) {
 	expected := []int{0, 1, 1}
 	actual := countBits(n)
 
-	if slices.Equal(expected, actual) {
-		t.Errorf("Expected and actual results are not equal.")
+	if !slices.Equal(expected, actual) {
+		t.Errorf("countBits(%d) returned incorrect result.", n)
 	}
 
 	// Example two
 	n = 5
-	expected = []int{0, 1, 2, 1, 2}
+	expected = []int{0, 1, 1, 2, 1, 2}
 	actual = countBits(n)
 
-	if slices.Equal(expected, actual) {
-		t.Errorf("Expected and actual results are not equal.")
+	if !slices.Equal(expected, actual) {
+		t.Errorf("countBits(%d) returned incorrect result.", n)
 	}
 }

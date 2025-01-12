0b0ac28b1e8ca7b12e9a4115c885a3d3c83aa4b9
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-12 12:29:25 +0100

    reverse bits - solution

--- golang/reversebits/reverse_bits.go
+++ golang/reversebits/reverse_bits.go
@@ -42,5 +42,12 @@
 package reversebits
 
 func reverseBits(num uint32) uint32 {
-	return num
+	var res uint32
+
+	for i := 0; i < 32; i++ {
+		res = res << 1 | (num & 1)
+		num >>= 1
+	}
+
+	return res
 }

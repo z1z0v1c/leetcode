907c96baac95de3ea2fb4625be11908a119c3462
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-14 00:52:21 +0100

    min stack - solution

--- golang/minstack/min_stack.go
+++ golang/minstack/min_stack.go
@@ -2,31 +2,31 @@
  * 155 - Medium
  *
  * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
- * 
+ *
  * Implement the MinStack class:
- * 
+ *
  * 	- MinStack() initializes the stack object.
  * 	- void push(int val) pushes the element val onto the stack.
  * 	- void pop() removes the element on the top of the stack.
  * 	- int top() gets the top element of the stack.
  * 	- int getMin() retrieves the minimum element in the stack.
- * 
+ *
  * You must implement a solution with O(1) time complexity for each function.
- *  
- * 
+ *
+ *
  * Example 1:
- * 
+ *
  * 	Input
- * 
+ *
  * 		["MinStack","push","push","push","getMin","pop","top","getMin"]
  * 		[[],[-2],[0],[-3],[],[],[],[]]
- * 
+ *
  * 	Output
- * 
+ *
  * 		[null,null,null,null,-3,null,0,-2]
- * 
+ *
  * 	Explanation
- * 
+ *
  * 		MinStack minStack = new MinStack();
  * 		minStack.push(-2);
  * 		minStack.push(0);
@@ -35,9 +35,9 @@
  * 		minStack.pop();
  * 		minStack.top();    // return 0
  * 		minStack.getMin(); // return -2
- * 
+ *
  * Constraints:
- * 
+ *
  * 	- -2^31 <= val <= 2^31 - 1
  * 	- Methods pop, top and getMin operations will always be called on non-empty stacks.
  * 	- At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
@@ -45,32 +45,42 @@
 package minstack
 
 type MinStack struct {
-    
+	min []int
+	stack []int
 }
 
 
 func Constructor() MinStack {
-	return MinStack{}
+	return MinStack{[]int{}, []int{}}
 }
 
 
-func (this *MinStack) Push(val int)  {
-    
+func (ms *MinStack) Push(val int)  {
+	ms.stack = append(ms.stack, val)
+	// Append element to min array if it's less or equal to the previous one
+	if len(ms.min) == 0 || ms.min[len(ms.min)-1] >= val {
+		ms.min = append(ms.min, val)
+	}
 }
 
 
-func (this *MinStack) Pop()  {
-    
+func (ms *MinStack) Pop()  {
+	top := ms.stack[len(ms.stack)-1]
+	ms.stack = ms.stack[:len(ms.stack)-1]
+	// Remove the top element from min array if present
+	if ms.min[len(ms.min)-1] == top {
+		ms.min = ms.min[:len(ms.min)-1]
+	}
 }
 
 
-func (this *MinStack) Top() int {
-	return 0
+func (ms *MinStack) Top() int {
+	return ms.stack[len(ms.stack)-1]
 }
 
 
-func (this *MinStack) GetMin() int {
-	return 0
+func (ms *MinStack) GetMin() int {
+	return ms.min[len(ms.min)-1]
 }
 
 

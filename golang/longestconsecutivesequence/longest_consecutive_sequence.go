9d614d72ba8b614fb484f719fe694ac30f2d0747
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-13 20:19:38 +0100

    longest consecutive sequence - first partial solution

--- golang/longestconsecutivesequence/longest_consecutive_sequence.go
+++ golang/longestconsecutivesequence/longest_consecutive_sequence.go
@@ -3,25 +3,46 @@
  *
  * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
  * You must write an algorithm that runs in O(n) time.
- * 
+ *
  * Example 1:
- * 
+ *
  * 	Input: nums = [100,4,200,1,3,2]
  * 	Output: 4
  * 	Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
- * 
+ *
  * Example 2:
- * 
+ *
  * 	Input: nums = [0,3,7,2,5,8,4,6,0,1]
  * 	Output: 9
- * 
+ *
  * Constraints:
- * 
+ *
  * 	- 0 <= nums.length <= 10^5
  * 	- -10^9 <= nums[i] <= 10^9
  */
 package longestconsecutivesequence
 
 func longestConsecutive(nums []int) int {
-	return len(nums)
+	sequences := make(map[int]int, len(nums))
+	longest := 0
+
+	for _, num := range nums {
+		if _, ok := sequences[num]; !ok {
+			// Get neighbor sequences
+			left := sequences[num-1]
+			right := sequences[num+1]
+
+			new := left + right + 1
+
+			// Update only boundaries
+			sequences[num-left] = new
+			sequences[num+right] = new
+
+			if new > longest {
+				longest = new
+			}
+		}
+	}
+
+	return longest
 }

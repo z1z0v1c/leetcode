5239030a94b97c6987da539c7b990907522e273d
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-13 20:21:27 +0100

    longest consecutive sequence - add third test

--- golang/longestconsecutivesequence/longest_consecutive_sequence_test.go
+++ golang/longestconsecutivesequence/longest_consecutive_sequence_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestLongestConsecutive(t *testing.T) {
 	// Example one
 	nums := []int{100, 4, 200, 1, 3, 2}
-	
+
 	expected := 4
 	actual := longestConsecutive(nums)
 
@@ -15,8 +15,18 @@ func TestLongestConsecutive(t *testing.T) {
 
 	// Example two
 	nums = []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}
-	
+
 	expected = 9
+	actual = longestConsecutive(nums)
+
+	if expected != actual {
+		t.Errorf("longestConsecutive(nums) returned %d instead of %d.", actual, expected)
+	}
+
+	// Example three
+	nums = []int{4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3}
+
+	expected = 5
 	actual = longestConsecutive(nums)
 
 	if expected != actual {

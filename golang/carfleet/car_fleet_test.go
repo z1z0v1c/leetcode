5da97e215db8ee2851dffae77e509775a2b4e68e
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-15 22:02:48 +0100

    car fleet - init commit

---
+++ golang/carfleet/car_fleet_test.go
@@ -0,0 +1,41 @@
+package carfleet
+
+import "testing"
+
+func TestCarFleet(t *testing.T) {
+	// Example one
+	target := 12
+	position := []int{10, 8, 0, 5, 3}
+	speed := []int{2, 4, 1, 1, 3}
+
+	expected := 3
+	actual := carFleet(target, position, speed)
+
+	if expected != actual {
+		t.Errorf("carFleet(%d, %#v, %#v) returned %d instead of %d.", target, position, speed, actual, expected)
+	}
+
+	// Example two
+	target = 10
+	position = []int{3}
+	speed = []int{3}
+
+	expected = 1
+	actual = carFleet(target, position, speed)
+
+	if expected != actual {
+		t.Errorf("carFleet(%d, %#v, %#v) returned %d instead of %d.", target, position, speed, actual, expected)
+	}
+
+	// Example three
+	target = 100
+	position = []int{0, 2, 4}
+	speed = []int{4, 2, 1}
+
+	expected = 1
+	actual = carFleet(target, position, speed)
+
+	if expected != actual {
+		t.Errorf("carFleet(%d, %#v, %#v) returned %d instead of %d.", target, position, speed, actual, expected)
+	}
+}

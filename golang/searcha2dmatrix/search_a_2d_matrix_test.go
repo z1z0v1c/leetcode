c63d5a6b7775d99695cbf31f3cc487367ac8da47
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-17 15:10:47 +0100

    search a 2d matrix - init commit

---
+++ golang/searcha2dmatrix/search_a_2d_matrix_test.go
@@ -0,0 +1,27 @@
+package searcha2dmatrix
+
+import "testing"
+
+func TestSearchMatrix(t *testing.T) {
+	// Example one
+	matrix, target := [][]int{
+		{1, 3, 5, 7},
+		{10, 11, 16, 20},
+		{23, 30, 34, 60},
+	}, 3
+	
+	if !searchMatrix(matrix, target) {
+		t.Errorf("searchMatrix(%#v, %d) should return true.", matrix, target)
+	}
+
+	// Example two
+	matrix, target = [][]int{
+		{1, 3, 5, 7},
+		{10, 11, 16, 20},
+		{23, 30, 34, 60},
+	}, 13
+	
+	if searchMatrix(matrix, target) {
+		t.Errorf("searchMatrix(%#v, %d) should return false.", matrix, target)
+	}
+}

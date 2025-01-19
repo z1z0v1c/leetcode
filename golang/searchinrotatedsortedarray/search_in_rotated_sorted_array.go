5356e68159fabdd655ad8477caa88f565134094a
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-19 01:52:08 +0100

    search in rotated sorted array - remove unused functions

--- golang/searchinrotatedsortedarray/search_in_rotated_sorted_array.go
+++ golang/searchinrotatedsortedarray/search_in_rotated_sorted_array.go
@@ -64,35 +64,3 @@ func search(nums []int, target int) int {
 
 	return -1
 }
-
-func bianarySearch(nums []int, target int) int {
-	start, end := 0, len(nums)-1
-
-	for start <= end {
-		mid := (start + end) / 2
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] < target {
-			start = mid + 1
-		} else {
-			end = mid - 1
-		}
-	}
-
-	return -1
-}
-
-func findMin(nums []int) int {
-	start, end := 0, len(nums)-1
-
-	for start < end {
-		mid := (start + end) / 2
-		if nums[mid] > nums[end] {
-			start = mid + 1
-		} else {
-			end = mid
-		}
-	}
-
-	return start
-}

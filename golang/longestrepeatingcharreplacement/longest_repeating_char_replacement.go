64b6b545daf96d71d518c3217da126cc05f8a7be
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-20 00:26:44 +0100

    longest repeating character replacement - solution

--- golang/longestrepeatingcharreplacement/longest_repeating_char_replacement.go
+++ golang/longestrepeatingcharreplacement/longest_repeating_char_replacement.go
@@ -3,29 +3,29 @@
  *
  * You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase
  * English character. You can perform this operation at most k times.
- * 
+ *
  * Return the length of the longest substring containing the same letter you can get after performing the above operations.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
+ *
  * 	Input: s = "ABAB", k = 2
  * 	Output: 4
  * 	Explanation: Replace the two 'A's with two 'B's or vice versa.
- * 
+ *
  * Example 2:
- * 
+ *
  * 	Input: s = "AABABBA", k = 1
  * 	Output: 4
- * 
+ *
  * 	Explanation:
- * 
+ *
  * 		Replace the one 'A' in the middle with 'B' and form "AABBBBA".
  * 		The substring "BBBB" has the longest repeating letters, which is 4.
  * 		There may exists other ways to achieve this answer too.
- * 
+ *
  * Constraints:
- * 
+ *
  * 	- 1 <= s.length <= 10^5
  * 	- s consists of only uppercase English letters.
  * 	- 0 <= k <= s.length
@@ -33,5 +33,30 @@
 package longestrepeatingcharreplacement
 
 func characterReplacement(s string, k int) int {
-	return len(s) + k
+	// Keep track of char frequences
+	fq := make(map[byte]int)
+	longest, maxfq := 0, 0
+
+	for start, end := 0, 0; end < len(s); end++ {
+		fq[s[end]]++
+
+		// Update max frequence
+		if fq[s[end]] > maxfq {
+			maxfq = fq[s[end]]
+		}
+
+		// Get current window size
+		size := end - start + 1
+
+		if size > maxfq + k {
+			// Move pointer and update frequency
+			fq[s[start]]--
+			start++
+		} else if longest < size {
+			// Update longest valid size
+			longest = size
+		}
+	}
+
+	return longest
 }
--- golang/longestrepeatingcharreplacement/longest_repeating_char_replacement_test.go
+++ golang/longestrepeatingcharreplacement/longest_repeating_char_replacement_test.go
@@ -22,4 +22,34 @@ func TestCharacterReplacement(t *testing.T) {
 	if expected != actual {
 		t.Errorf("characterReplacement(%s, %d) returned %d instead of %d.", s, k, actual, expected)
 	}
+
+	// Example three
+	s, k = "ABAA", 0
+
+	expected = 2
+	actual = characterReplacement(s, k)
+
+	if expected != actual {
+		t.Errorf("characterReplacement(%s, %d) returned %d instead of %d.", s, k, actual, expected)
+	}
+
+	// Example four
+	s, k = "ABBB", 2
+
+	expected = 4
+	actual = characterReplacement(s, k)
+
+	if expected != actual {
+		t.Errorf("characterReplacement(%s, %d) returned %d instead of %d.", s, k, actual, expected)
+	}	
+	
+	// Example four
+	s, k = "BAAAB", 2
+
+	expected = 5
+	actual = characterReplacement(s, k)
+
+	if expected != actual {
+		t.Errorf("characterReplacement(%s, %d) returned %d instead of %d.", s, k, actual, expected)
+	}
 }

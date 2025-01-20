478770cfd10048028be4db4b28a9aa330b25671e
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-20 02:49:49 +0100

    reorder list - init commit

---
+++ golang/reorderlist/reorder_list_test.go
@@ -0,0 +1,82 @@
+package reorderlist
+
+import (
+	"reflect"
+	"testing"
+
+	cs "github.com/z1z0v1c/leetcode/commonstructs"
+)
+
+func TestReorderList(t *testing.T) {
+	// Example one
+	head := &cs.ListNode{
+		Val: 1,
+		Next: &cs.ListNode{
+			Val: 2,
+			Next: &cs.ListNode{
+				Val: 3,
+				Next: &cs.ListNode{
+					Val: 4,
+				},
+			},
+		},
+	}
+	
+	expected := &cs.ListNode{
+		Val: 1,
+		Next: &cs.ListNode{
+			Val: 4,
+			Next: &cs.ListNode{
+				Val: 2,
+				Next: &cs.ListNode{
+					Val: 3,
+				},
+			},
+		},
+	}
+
+	reorderList(head)
+
+	if !reflect.DeepEqual(head, expected) {
+		t.Error("reorderList(head) returned incorrect resut for example 1.")
+	}
+
+	// Example two
+	head = &cs.ListNode{
+		Val: 1,
+		Next: &cs.ListNode{
+			Val: 2,
+			Next: &cs.ListNode{
+				Val: 3,
+				Next: &cs.ListNode{
+					Val: 4,
+					Next: &cs.ListNode{
+						Val: 5,
+					},
+				},
+			},
+		},
+	}
+	
+	expected = &cs.ListNode{
+		Val: 1,
+		Next: &cs.ListNode{
+			Val: 5,
+			Next: &cs.ListNode{
+				Val: 2,
+				Next: &cs.ListNode{
+					Val: 4,
+					Next: &cs.ListNode{
+						Val: 3,
+					},
+				},
+			},
+		},
+	}
+
+	reorderList(head)
+
+	if !reflect.DeepEqual(head, expected) {
+		t.Error("reorderList(head) returned incorrect resut for example 1.")
+	}
+}

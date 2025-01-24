086baad550c7d2a28c9919be3d0b8b754b65ee6a
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-24 21:01:45 +0100

    construct binary tree from preorder and inorder traversal - refactored solution

--- golang/construct_binary_tree_prein/construct_binary_tree_prein.go
+++ golang/construct_binary_tree_prein/construct_binary_tree_prein.go
@@ -3,19 +3,19 @@
  *
  * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and
  * inorder is the inorder traversal of the same tree, construct and return the binary tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * 	Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
  * 	Output: [3,9,20,null,null,15,7]
- * 
+ *
  * Example 2:
- * 
+ *
  * 	Input: preorder = [-1], inorder = [-1]
  * 	Output: [-1]
- * 
+ *
  * Constraints:
- * 
+ *
  * 	- 1 <= preorder.length <= 3000
  * 	- inorder.length == preorder.length
  * 	- -3000 <= preorder[i], inorder[i] <= 3000
@@ -26,24 +26,20 @@
  */
 package constructbinarytreeprein
 
-import cs "github.com/z1z0v1c/leetcode/commonstructs"
+import (
+	"slices"
+
+	cs "github.com/z1z0v1c/leetcode/commonstructs"
+)
 
 func buildTree(preorder []int, inorder []int) *cs.TreeNode {
 	if len(preorder) == 0 {
 		return nil
 	}
 
-	rootVal := preorder[0]
+	root := &cs.TreeNode{Val: preorder[0]}
 
-	var i int
-	for j := 0; j < len(inorder); j++ {
-		if rootVal == inorder[j] {
-			i = j
-			break
-		}
-	} 
-	
-	root := &cs.TreeNode{Val: rootVal}
+	i := slices.Index(inorder, preorder[0])
 
 	root.Left = buildTree(preorder[1:i+1], inorder[:i])
 	root.Right = buildTree(preorder[i+1:], inorder[i+1:])

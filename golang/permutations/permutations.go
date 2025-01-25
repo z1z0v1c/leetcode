beb9f05974d60f14f3219da2aa180bf14d9f38dc
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-01-25 20:34:10 +0100

    permutations - solution

--- golang/permutations/permutations.go
+++ golang/permutations/permutations.go
@@ -3,24 +3,24 @@
  *
  * Given an array nums of distinct integers, return all the possible permutations
  * You can return the answer in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * 	Input: nums = [1,2,3]
  * 	Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
- * 
+ *
  * Example 2:
- * 
+ *
  * 	Input: nums = [0,1]
  * 	Output: [[0,1],[1,0]]
- * 
+ *
  * Example 3:
- * 
+ *
  * 	Input: nums = [1]
  * 	Output: [[1]]
- * 
+ *
  * Constraints:
- * 
+ *
  * 	- 1 <= nums.length <= 6
  * 	- -10 <= nums[i] <= 10
  * 	- All the integers of nums are unique.
@@ -30,5 +30,31 @@ package permutations
 func permute(nums []int) [][]int {
 	permutations := [][]int{}
 
+	backtrack(&permutations, nums, []int{})
+
 	return permutations
+}
+
+func backtrack(permutations *[][]int, nums, permutation []int) {
+	if len(nums) == 0 {
+		// Allocate new memory to avoid bugs
+		new := make([]int, 0, len(permutation))
+		new = append(new, permutation...)
+		
+		*permutations = append(*permutations, new)
+	} else {
+		for i := 0; i < len(nums); i++ {
+			permutation = append(permutation, nums[i])
+
+			// Create new nums without used element
+			newNums := make([]int, 0, len(nums)-1)
+			newNums = append(newNums, nums[:i]...)
+			newNums = append(newNums, nums[i+1:]...)
+
+			backtrack(permutations, newNums, permutation)
+
+			// Remove the last element after recursive call
+			permutation = permutation[:len(permutation)-1]
+		}
+	}
 }
--- golang/permutations/permutations_test.go
+++ golang/permutations/permutations_test.go
@@ -9,7 +9,7 @@ func TestPermute(t *testing.T) {
 	// Example one
 	nums := []int{1, 2, 3}
 
-	expected := [][]int{{1,2,3},{1,3,2},{2,1,3},{2,3,1},{3,1,2},{3,2,1}}
+	expected := [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}
 	actual := permute(nums)
 
 	if !reflect.DeepEqual(expected, actual) {
@@ -19,7 +19,7 @@ func TestPermute(t *testing.T) {
 	// Example two
 	nums = []int{0, 1}
 
-	expected = [][]int{{0, 1},{1, 0}}
+	expected = [][]int{{0, 1}, {1, 0}}
 	actual = permute(nums)
 
 	if !reflect.DeepEqual(expected, actual) {

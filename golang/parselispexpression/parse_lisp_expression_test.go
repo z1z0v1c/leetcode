fb4858fd6387bf87019242fca369089c46c43fc0
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-02-03 23:53:54 +0100

    parse lisp expression - init commit

---
+++ golang/parselispexpression/parse_lisp_expression_test.go
@@ -0,0 +1,35 @@
+package parselispexpression
+
+import "testing"
+
+func TestEvaluate(t *testing.T) {
+	// Example one
+	expression := "(let x 2 (mult x (let x 3 y 4 (add x y))))"
+
+	expected := 14
+	actual := evaluate(expression)
+
+	if expected != actual {
+		t.Errorf("evaluate(%s) returned %d instead of %d.", expression, actual, expected)
+	}
+
+	// Example two
+	expression = "(let x 3 x 2 x)"
+
+	expected = 2
+	actual = evaluate(expression)
+
+	if expected != actual {
+		t.Errorf("evaluate(%s) returned %d instead of %d.", expression, actual, expected)
+	}
+
+	// Example three
+	expression = "(let x 1 y 2 x (add x y) (add x y))"
+
+	expected = 5
+	actual = evaluate(expression)
+
+	if expected != actual {
+		t.Errorf("evaluate(%s) returned %d instead of %d.", expression, actual, expected)
+	}
+}

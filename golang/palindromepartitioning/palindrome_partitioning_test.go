8f3b74cdbfea4995962f7b4edd752b47bddd022a
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-02-09 22:59:48 +0100

    palindrome partitioning - init commit

---
+++ golang/palindromepartitioning/palindrome_partitioning_test.go
@@ -0,0 +1,28 @@
+package palindromepartitioning
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPartition(t *testing.T) {
+	// Example one
+	s := "aab"
+
+	expected := [][]string{{"a", "a", "b"}, {"aa", "b"}}
+	actual := partition(s)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("partition(%s) returned %#v instead of %#v.", s, actual, expected)
+	}
+
+	// Example two
+	s = "a"
+
+	expected = [][]string{{"a"}}
+	actual = partition(s)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("partition(%s) returned %#v instead of %#v.", s, actual, expected)
+	}
+}

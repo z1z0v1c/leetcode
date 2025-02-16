ed6b72e5d49ff389612c1cbefd1a0d7a6c6be6a6
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-02-16 21:25:59 +0100

    house robber - solution

---
+++ golang/houserobber/house_robber.go
@@ -0,0 +1,76 @@
+/*
+*
+  - 198 - Medium
+    *
+  - You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed,
+
+the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it
+will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob
+tonight without alerting the police.
+
+Example 1:
+
+	Input: nums = [1,2,3,1]
+	Output: 4
+
+	Explanation:
+
+		- Rob house 1 (money = 1) and then rob house 3 (money = 3).
+		- Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+
+	Input: nums = [2,7,9,3,1]
+	Output: 12
+
+	Explanation:
+
+		- Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+		- Total amount you can rob = 2 + 9 + 1 = 12.
+
+Constraints:
+
+  - 1 <= nums.length <= 100
+  - 0 <= nums[i] <= 400
+*/
+package houserobber
+
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	mem := make([]int, len(nums)) 
+
+	mem[0] = nums[0]
+	mem[1] = nums[1]
+
+	best, prevBest := max(mem[0], mem[1]), 0
+
+	for i := 2; i < len(nums); i++ {
+		if best > mem[i-2] && best != mem[i-1] {
+			mem[i] = nums[i] + best
+		} else if prevBest > mem[i-2] {
+			mem[i] = nums[i] + prevBest
+		} else {
+			mem[i] = nums[i] + mem[i-2]
+		}
+
+		if mem[i] >= best {
+			prevBest = best
+			best = mem[i]
+		}
+	}
+
+	return best
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
---
+++ golang/houserobber/house_robber_test.go
@@ -0,0 +1,61 @@
+package houserobber
+
+import "testing"
+
+func TestRob(t *testing.T) {
+	// Example one
+	nums := []int{1, 2, 3, 1}
+
+	expected := 4
+	actual := rob(nums)
+
+	if expected != actual {
+		t.Errorf("rob(%#v) returned %d instead of %d.", nums, actual, expected)
+	}
+
+	// Example two
+	nums = []int{2, 7, 9, 3, 1}
+
+	expected = 12
+	actual = rob(nums)
+
+	if expected != actual {
+		t.Errorf("rob(%#v) returned %d instead of %d.", nums, actual, expected)
+	}
+
+	// Example three
+	nums = []int{0}
+
+	expected = 0
+	actual = rob(nums)
+
+	if expected != actual {
+		t.Errorf("rob(%#v) returned %d instead of %d.", nums, actual, expected)
+	}
+
+	// Example four
+	nums = []int{2, 1, 1, 2}
+
+	expected = 4
+	actual = rob(nums)
+
+	if expected != actual {
+		t.Errorf("rob(%#v) returned %d instead of %d.", nums, actual, expected)
+	}
+
+	// Example five
+	nums = []int{
+        228, 67, 195, 15, 0, 90, 151, 210, 17, 196, 0, 10, 28, 110, 169, 94, 9, 23, 52, 63,
+        136, 122, 22, 191, 144, 22, 173, 106, 88, 59, 200, 156, 39, 109, 244, 231, 183, 99,
+        114, 15, 114, 32, 57, 36, 117, 151, 177, 106, 229, 188, 178, 47, 96, 191, 25, 180, 153,
+        187, 136, 146, 117, 57, 77, 110, 215, 115, 84, 218, 59, 121, 202, 109, 205, 95, 214,
+        100, 175, 50, 223, 11, 14, 164, 224, 15, 100, 241, 61, 64, 197, 206, 3, 149, 108, 186,
+    }
+
+	expected = 6762
+	actual = rob(nums)
+
+	if expected != actual {
+		t.Errorf("rob(%#v) returned %d instead of %d.", nums, actual, expected)
+	}
+}

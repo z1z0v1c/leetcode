d6d42d7efb83cb1e9228c6ff378a80b98b27e4a7
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-02-19 11:51:00 +0100

    decode ways - init commit

---
+++ golang/decodeways/decode_ways_test.go
@@ -0,0 +1,35 @@
+package decodeways
+
+import "testing"
+
+func TestNumDecodings(t *testing.T) {
+	// Example one
+	s := "12"
+
+	expected := 2
+	actual := numDecodings(s)
+
+	if expected != actual {
+		t.Errorf("numDecodings(%s) returned %d instead of %d.", s, actual, expected)
+	}
+
+	// Example two
+	s = "226"
+
+	expected = 3
+	actual = numDecodings(s)
+
+	if expected != actual {
+		t.Errorf("numDecodings(%s) returned %d instead of %d.", s, actual, expected)
+	}
+
+	// Example three
+	s = "06"
+
+	expected = 0
+	actual = numDecodings(s)
+
+	if expected != actual {
+		t.Errorf("numDecodings(%s) returned %d instead of %d.", s, actual, expected)
+	}
+}

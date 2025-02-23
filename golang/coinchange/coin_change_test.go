0d477babde1f635b5aa6dccd37bc3a2f0584eb17
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-02-23 01:21:46 +0100

    coin change - init commit

---
+++ golang/coinchange/coin_change_test.go
@@ -0,0 +1,38 @@
+package coinchange
+
+import "testing"
+
+func TestCoinChange(t *testing.T) {
+	// Example one
+	coins := []int{1, 2, 5}
+	amount := 11
+
+	expected := 3
+	actual := coinChange(coins, amount)
+
+	if expected != actual {
+		t.Errorf("coinChange(%#v, %d) returned %d instead of %d.", coins, amount, actual, expected)
+	}
+
+	// Example two
+	coins = []int{2}
+	amount = 3
+
+	expected = -1
+	actual = coinChange(coins, amount)
+
+	if expected != actual {
+		t.Errorf("coinChange(%#v, %d) returned %d instead of %d.", coins, amount, actual, expected)
+	}
+
+	// Example three
+	coins = []int{1}
+	amount = 0
+
+	expected = 0
+	actual = coinChange(coins, amount)
+
+	if expected != actual {
+		t.Errorf("coinChange(%#v, %d) returned %d instead of %d.", coins, amount, actual, expected)
+	}
+}

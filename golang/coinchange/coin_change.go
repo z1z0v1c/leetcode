458b367a7222be864ae2b9e6b492d96430611373
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-02-25 04:04:23 +0100

    coin change - refactored solution

--- golang/coinchange/coin_change.go
+++ golang/coinchange/coin_change.go
@@ -1,8 +1,8 @@
 /**
  * 322 - Medium
  *
- * You are given an integer array coins representing coins of different denominations and an integer amount
- * representing a total amount of money.
+ * You are given an integer array coins repchangeenting coins of different denominations and an integer amount
+ * repchangeenting a total amount of money.
  *
  * Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made
  * up by any combination of the coins, return -1.
@@ -39,25 +39,32 @@ import "sort"
 func coinChange(coins []int, amount int) int {
 	sort.Ints(coins)
 
-	mem := make([]int, amount+1)
-	mem[0] = 0
+	minCoins := make([]int, amount+1)
+	minCoins[0] = 0
 
 	for i := 1; i <= amount; i++ {
-		mem[i] = -1
-	}
-
-	for i := 1; i <= amount; i++ {
-		for j := 0; j < len(coins); j++ {
-			res := i - coins[j]
-			if res >= 0 {
-				if (mem[i] == -1 || mem[i] > mem[res] + 1) && mem[res] != -1 {
-					mem[i] = mem[res] + 1
-				}
+		minCoins[i] = amount + 1
+		for _, coin := range coins {
+			change := i - coin
+			if change >= 0 {
+				minCoins[i] = min(minCoins[i], minCoins[change]+1)
 			} else {
 				break
 			}
 		}
 	}
 
-	return mem[amount]
+	if minCoins[amount] <= amount {
+		return minCoins[amount]
+	}
+
+	return -1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }

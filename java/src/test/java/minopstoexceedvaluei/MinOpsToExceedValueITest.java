8b2839a632d063b97235b569da92be835670a296
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-05-08 01:42:45 +0200

    minimum operations to exceed threshold value i

---
+++ java/src/test/java/minopstoexceedvaluei/MinOpsToExceedValueITest.java
@@ -0,0 +1,52 @@
+package minopstoexceedvaluei;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MinOpsToExceedValueITest {
+    private MinOpsToExceedValueI minOps;
+
+    @BeforeEach
+    void setUp() {
+        minOps = new MinOpsToExceedValueI();
+    }
+
+    @Test
+    @DisplayName("Example one")
+    void testMinOperationsExampleOne() {
+        int k = 10;
+        int[] nums = new int[] { 2, 11, 10, 1, 3 };
+
+        int expected = 3;
+        int actual = minOps.minOperations(nums, k);
+
+        assertEquals(expected, actual, String.format("Should return %d", expected));
+    }
+
+    @Test
+    @DisplayName("Example two")
+    void testMinOperationsExampleTwo() {
+        int k = 1;
+        int[] nums = new int[] { 1, 1, 2, 4, 9 };
+
+        int expected = 0;
+        int actual = minOps.minOperations(nums, k);
+
+        assertEquals(expected, actual, String.format("Should return %d", expected));
+    }
+
+    @Test
+    @DisplayName("Example three")
+    void testMinOperationsExampleThree() {
+        int k = 9;
+        int[] nums = new int[] { 1, 1, 2, 4, 9 };
+
+        int expected = 4;
+        int actual = minOps.minOperations(nums, k);
+
+        assertEquals(expected, actual, String.format("Should return %d", expected));
+    }
+}

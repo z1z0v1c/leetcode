050baa0f16e657698b598804b8756548817be6f8
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-05-08 13:40:34 +0200

    minimum right shifts to sort the array

---
+++ java/src/test/java/minrightshiftstosortarray/MinRightShiftsToSortArrayTest.java
@@ -0,0 +1,52 @@
+package minrightshiftstosortarray;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MinRightShiftsToSortArrayTest {
+    private MinRightShiftsToSortArray minShifts;
+
+    @BeforeEach
+    void setUp() {
+        minShifts = new MinRightShiftsToSortArray();
+    }
+
+    @Test
+    @DisplayName("Example one")
+    void testMinOperationsExampleOne() {
+        List<Integer> nums = Arrays.asList(new Integer[] { 3, 4, 5, 1, 2 });
+
+        int expected = 2;
+        int actual = minShifts.minimumRightShifts(nums);
+
+        assertEquals(expected, actual, String.format("Should return %d", expected));
+    }
+
+    @Test
+    @DisplayName("Example two")
+    void testMinOperationsExampleTwo() {
+        List<Integer> nums = Arrays.asList(new Integer[] { 1, 3, 5 });
+
+        int expected = 0;
+        int actual = minShifts.minimumRightShifts(nums);
+
+        assertEquals(expected, actual, String.format("Should return %d", expected));
+    }
+
+    @Test
+    @DisplayName("Example three")
+    void testMinOperationsExampleThree() {
+        List<Integer> nums = Arrays.asList(new Integer[] { 2, 1, 4 });
+
+        int expected = -1;
+        int actual = minShifts.minimumRightShifts(nums);
+
+        assertEquals(expected, actual, String.format("Should return %d", expected));
+    }
+}

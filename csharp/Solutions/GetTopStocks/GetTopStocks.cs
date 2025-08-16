43631ac3bd634667aed32e935bfc6e1c31ea593a
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-16 17:11:29 +0200

    get top stocks - solution

--- csharp/Solutions/GetTopStocks/GetTopStocks.cs
+++ csharp/Solutions/GetTopStocks/GetTopStocks.cs
@@ -11,7 +11,7 @@
 /// 
 /// An example input would look like this:
 ///     ["AMZN", "CACC", "EQIXS", "GOOG", "ORLY", "ULTA"]
-///     [12.81, 11.09, 12.11, 10.93, 9.83, 8.14], [10.34, 10.56, 10.14, 12.17, 9.66, 8,90]
+///     [11.93, 11.09, 12.11, 10.93, 9.83, 8.14], [10.34, 10.56, 10.14, 12.17, 13.12, 8,90]
 /// 
 /// Your GetTopStocks method should return an array containing the names of the three
 /// stocks with the highest average value. The array should be sorted by decreasing
@@ -27,11 +27,33 @@ public class GetTopStocksSolution
 {
     public string[] GetTopStocks(string[] stocks, double[][] prices)
     {
-        string[] topStocks = new string[prices[0].length];
+        var topStocks = new string[3];
+        var average = new double[stocks.Length];
 
-        for (int i = 0; i < stocks.length; i++)
+        for (int i = 0; i < stocks.Length; i++)
         {
-            topStocks[i] = stocks[i];
+            var sum = 0d;
+            for (int j = 0; j < prices.Length; j++)
+            {
+                sum += prices[j][i];
+            }
+
+            average[i] = sum / prices.Length;
+        }
+
+        var orderedStocks = new Dictionary<double, string>();
+
+        for (int i = 0; i < stocks.Length; i++)
+        {
+            orderedStocks.Add(average[i], stocks[i]);
+        }
+
+        List<double> ordered = [.. orderedStocks.Keys];
+        ordered.Sort();
+
+        for (int i = 0; i < 3; i++)
+        {
+            topStocks[i] = orderedStocks.GetValueOrDefault(ordered.ElementAt(stocks.Length - i - 1))!;
         }
 
         return topStocks;
--- csharp/Tests/GetTopStocksTests/GetTopStocksTests.cs
+++ csharp/Tests/GetTopStocksTests/GetTopStocksTests.cs
@@ -1,8 +1,10 @@
+using Solutions.GetTopStocks;
+
 namespace Tests.GetTopStocksTests;
 
 public class GetTopStocksTests
 {
-    private GetTopStocks getTopStocks;
+    private GetTopStocksSolution getTopStocks;
 
     [SetUp]
     public void Setup() => getTopStocks = new();
@@ -10,14 +12,14 @@ public class GetTopStocksTests
     [Test]
     public void TestExampleOne()
     {
-        var stocks = new string { "AMZN", "CACC", "EQIXS", "GOOG", "ORLY", "ULTA" };
-        var prices = new double
+        var stocks = new string[] { "AMZN", "CACC", "EQIXS", "GOOG", "ORLY", "ULTA" };
+        var prices = new double[][]
         {
-            {12.81, 11.09, 12.11, 10.93, 9.83, 8.14},
-            {10.34, 10.56, 10.14, 12.17, 9.66, 8,90}
+            [11.93, 11.09, 12.11, 10.93, 9.83, 8.14],
+            [10.34, 10.56, 10.14, 12.17, 13.12, 8,90]
         };
 
-        var expected = { "GOOG", "ORLY", "AMZN" };
+        var expected = new string[] { "GOOG", "ORLY", "AMZN" };
         var actual = getTopStocks.GetTopStocks(stocks, prices);
 
         Assert.That(actual, Is.EqualTo(expected));

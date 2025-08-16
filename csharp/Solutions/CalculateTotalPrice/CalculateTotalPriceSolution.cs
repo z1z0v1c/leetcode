a207c85aa69c0aefa7e07c3a23db5a3c8f4cd3b6
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-16 23:22:39 +0200

    calculate total prices - init commit

---
+++ csharp/Solutions/CalculateTotalPrice/CalculateTotalPriceSolution.cs
@@ -0,0 +1,24 @@
+/// <sumary>
+///
+/// You work for a shop that wishes to give a discount of discount% to the most expensive item purchased
+/// by a given customer during the sales period. Only one product can benefit from the discount.
+///
+/// You are tasked by the shop owner to implement the method CalculateTotalPrice(prices, discount) which
+/// takes the list of prices of the products purchased by the customer end the percentage 'discount' as
+/// parameters and returns the total purchase price as an integer (rounded down if the total is a float
+/// number.
+///
+/// Constraints:
+///     - 0 <= discount <= 100
+///     - 0 < price of a product < 100000
+///     - 0 < number of products < 100
+/// </sumary>
+namespace Solutions.CalculateTotalPrice;
+
+public class CalculateTotalPriceSolution
+{
+    public int CalculateTotalPrice(int[] prices, int discount)
+    {
+        return 0;
+    }
+}
--- csharp/Tests/GetTopStocksTests/GetTopStocksTests.cs
+++ csharp/Tests/GetTopStocksTests/GetTopStocksTests.cs
@@ -13,13 +13,13 @@ public class GetTopStocksTests
     public void TestExampleOne()
     {
         // Arrange
-        var stocks = new string[] { "AMZN", "CACC", "EQIXS", "GOOG", "ORLY", "ULTA" };
+        var stocks = new[] { "AMZN", "CACC", "EQIXS", "GOOG", "ORLY", "ULTA" };
         var prices = new double[][]
         {
             [11.93, 11.09, 12.11, 10.93, 9.83, 8.14],
             [10.34, 10.56, 10.14, 12.17, 13.12, 8,90]
         };
-        var expected = new string[] { "GOOG", "ORLY", "AMZN" };
+        var expected = new[] { "GOOG", "ORLY", "AMZN" };
         
         // Act
         var actual = getTopStocks.GetTopStocks(stocks, prices);

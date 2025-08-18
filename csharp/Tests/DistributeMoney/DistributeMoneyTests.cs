fc8353b6eb419f00c9064babfee1eaeb1319da71
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-18 22:35:54 +0200

    distribute money - init commit

---
+++ csharp/Tests/DistributeMoney/DistributeMoneyTests.cs
@@ -0,0 +1,41 @@
+using Solutions.DistributeMoney;
+
+namespace Tests.DistributeMoney;
+
+public class DistributeMoneyTests
+{
+    private DistributeMoneySolution solution;
+
+    [SetUp]
+    public void Setup() => solution = new();
+
+    [Test]
+    public void TestExampleOne()
+    {
+        // Arrange
+        var money = 20;
+        var children = 3;
+        var expected = 1;
+        
+        // Act
+        var actual = solution.DistMoney(money,  children);
+        
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        // Arrange
+        var money = 16;
+        var children = 2;
+        var expected = 2;
+        
+        // Act
+        var actual = solution.DistMoney(money,  children);
+        
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}

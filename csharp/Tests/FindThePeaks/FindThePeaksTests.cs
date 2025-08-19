19324fb9b90afb3d779d021a066c935ca4ee7a27
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-19 04:24:20 +0200

    find the peaks - init commit

---
+++ csharp/Tests/FindThePeaks/FindThePeaksTests.cs
@@ -0,0 +1,39 @@
+using Solutions.FindThePeaks;
+
+namespace Tests.FindThePeaks;
+
+public class FindThePeaksTests
+{
+    private FindThePeaksSolution solution;
+
+    [SetUp]
+    public void Setup() => solution = new();
+
+    [Test]
+    public void TestExampleOne()
+    {
+        // Arrange
+        var mountain = new[] { 2, 4, 4 };
+        var expected = new Lazy<int>();
+
+        // Act
+        var actual = solution.FindPeaks(mountain);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        // Arrange
+        var mountain = new[] { 1, 4, 3, 8, 5 };
+        var expected = new List<int> { 1, 3 };
+
+        // Act
+        var actual = solution.FindPeaks(mountain);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}

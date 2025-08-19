48c95d5e43c6f4cdec82646019780f18a97e98e8
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-19 05:00:07 +0200

    peak index in a mountain array - add the fourt test

--- csharp/Tests/PeakIndexInAnArray/PeakIndexInAnArrayTests.cs
+++ csharp/Tests/PeakIndexInAnArray/PeakIndexInAnArrayTests.cs
@@ -15,38 +15,52 @@ public class PeakIndexInAnArrayTests
         // Arrange
         var arr = new[] { 0, 1, 0 };
         var expected = 1;
-        
+
         // Act
         var actual = solution.PeakIndexInMountainArray(arr);
-        
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
-    
+
     [Test]
     public void TestExampleTwo()
     {
         // Arrange
         var arr = new[] { 0, 2, 1, 0 };
         var expected = 1;
-        
+
         // Act
         var actual = solution.PeakIndexInMountainArray(arr);
-        
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
-    
+
     [Test]
     public void TestExampleThree()
     {
         // Arrange
         var arr = new[] { 0, 10, 5, 2 };
         var expected = 1;
-        
+
         // Act
         var actual = solution.PeakIndexInMountainArray(arr);
-        
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleFour()
+    {
+        // Arrange
+        var arr = new[] { 40, 48, 61, 75, 100, 99, 98, 39, 30, 10 };
+        var expected = 4;
+
+        // Act
+        var actual = solution.PeakIndexInMountainArray(arr);
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }

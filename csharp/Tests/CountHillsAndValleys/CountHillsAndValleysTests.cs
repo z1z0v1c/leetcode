ffee62080e3d82662074fa6d7f700b75e5777a04
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-19 03:52:25 +0200

    caunt hills and valleys in an array - add the third test

--- csharp/Tests/CountHillsAndValleys/CountHillsAndValleysTests.cs
+++ csharp/Tests/CountHillsAndValleys/CountHillsAndValleysTests.cs
@@ -13,26 +13,44 @@ public class CountHillsAndValleysTests
     public void TestExampleOne()
     {
         // Arrange
-        var nums = new[]{ 2, 4, 1, 1, 6, 5 };
+        var nums = new[] { 2, 4, 1, 1, 6, 5 };
         var expected = 3;
-        
+
         // Act
         var actual = solution.CountHillValley(nums);
-        
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
-    
+
     [Test]
     public void TestExampleTwo()
     {
         // Arrange
-        var nums = new[]{ 6, 6, 5, 5, 4, 1 };
+        var nums = new[] { 6, 6, 5, 5, 4, 1 };
         var expected = 0;
-        
+
         // Act
         var actual = solution.CountHillValley(nums);
-        
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleThree()
+    {
+        // Arrange
+        var nums = new[]
+        {
+            57, 57, 57, 57, 57, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 85, 85, 85, 86, 86,
+            86
+        };
+        var expected = 2;
+
+        // Act
+        var actual = solution.CountHillValley(nums);
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }

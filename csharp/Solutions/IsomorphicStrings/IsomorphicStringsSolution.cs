9879891e707f59ebff61d7c82ac8acd64aa95068
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-20 22:15:35 +0200

    isomorphic strings - init solution

--- csharp/Solutions/IsomorphicStrings/IsomorphicStringsSolution.cs
+++ csharp/Solutions/IsomorphicStrings/IsomorphicStringsSolution.cs
@@ -35,6 +35,41 @@ public class IsomorphicStringsSolution
 {
     public bool IsIsomorphic(string s, string t)
     {
-        return false;
+        var sDict = new Dictionary<char, int>();
+        var tDict = new Dictionary<char, int>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (!sDict.ContainsKey(s[i]))
+            {
+                sDict[s[i]] = 1;
+            }
+            
+            if (!tDict.ContainsKey(t[i]))
+            {
+                tDict[t[i]] = 1;
+            }
+            
+            sDict[s[i]]++;
+            tDict[t[i]]++;
+        }
+        
+        var sValues = sDict.Values.ToArray();
+        var tValues = tDict.Values.ToArray();
+
+        if (sValues.Length != tValues.Length)
+        {
+            return false;
+        }
+
+        for (var i = 0; i < sValues.Length; i++)
+        {
+            if (sValues[i] != tValues[i])
+            {
+                return false;
+            }
+        }
+        
+        return true;
     } 
 }

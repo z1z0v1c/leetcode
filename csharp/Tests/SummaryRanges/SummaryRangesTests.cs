5b5c1809a485a4c0724e3604defd02279bad46b0
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-23 16:57:08 +0200

    summary ranges - add fourth test

--- csharp/Tests/SummaryRanges/SummaryRangesTests.cs
+++ csharp/Tests/SummaryRanges/SummaryRangesTests.cs
@@ -17,11 +17,11 @@ public class SummaryRangesTests
     {
         // Arrange
         var nums = new[] { 0, 1, 2, 4, 5, 7 };
-        var expected = new [] { "0->2", "4->5", "7" };
-        
+        var expected = new[] { "0->2", "4->5", "7" };
+
         // Act
         var actual = solution.SummaryRanges(nums);
-        
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
@@ -31,25 +31,39 @@ public class SummaryRangesTests
     {
         // Arrange
         var nums = new[] { 0, 2, 3, 4, 6, 8, 9 };
-        var expected = new [] { "0", "2->4", "6", "8->9" };
-        
+        var expected = new[] { "0", "2->4", "6", "8->9" };
+
         // Act
         var actual = solution.SummaryRanges(nums);
-        
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
-    
+
     [Test]
     public void TestExampleThree()
     {
         // Arrange
         int[] nums = [];
         string[] expected = [];
-        
+
         // Act
         var actual = solution.SummaryRanges(nums);
-        
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleFour()
+    {
+        // Arrange
+        var nums = new[] { -2147483648, 0, 2, 3, 4, 6, 8, 9 };
+        var expected = new[] { "-2147483648", "0", "2->4", "6", "8->9" };
+
+        // Act
+        var actual = solution.SummaryRanges(nums);
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }

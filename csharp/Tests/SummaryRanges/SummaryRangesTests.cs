3a638d725feb7401008c98620b885bffaacaff5d
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-23 15:32:45 +0200

    summary ranges - init commit

---
+++ csharp/Tests/SummaryRanges/SummaryRangesTests.cs
@@ -0,0 +1,42 @@
+using Solutions.SummaryRanges;
+
+namespace Tests.SummaryRanges;
+
+public class SummaryRangesTests
+{
+    private SummaryRangesSolution solution;
+
+    [SetUp]
+    public void Setup()
+    {
+        solution = new SummaryRangesSolution();
+    }
+
+    [Test]
+    public void TestExampleOne()
+    {
+        // Arrange
+        var nums = new[] { 0, 1, 2, 4, 5, 7 };
+        var expected = new [] { "0->2", "4->5", "7" };
+        
+        // Act
+        var actual = solution.SummaryRanges(nums);
+        
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestExampleTwo()
+    {
+        // Arrange
+        var nums = new[] { 0, 2, 3, 4, 6, 8, 9 };
+        var expected = new [] { "0->2", "4->5", "7" };
+        
+        // Act
+        var actual = solution.SummaryRanges(nums);
+        
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
